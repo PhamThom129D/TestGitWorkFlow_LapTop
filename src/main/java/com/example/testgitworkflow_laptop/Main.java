0784e8e25365e2d668915256d030df931f79c994
T14 <vykid9@gmail.com>
2024-10-15 10:33:34 +0700

    CRUD_Laptop

--- src/main/java/com/example/testgitworkflow_laptop/Main.java
+++ src/main/java/com/example/testgitworkflow_laptop/Main.java
@@ -4,8 +4,18 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+        CRUD_Laptop crud = new CRUD_Laptop();
         ShowList showList = new ShowList();
         String query = "Select * from Laptop";
         showList.showList(query);
+
+        String query2 = "insert into Laptop(name,RAM,SSD,chipset,price,stock)"+ "values(?,?,?,?,?,?)";
+        crud.addLaptop(query2,"ThinkPad T14s","12GB","256","i7",100.5,true);
+
+        String query3 = "Update Laptop set name = ? where id = ?";
+        crud.updateLaptop(query3,1,"ThinkPad update");
+
+        String query4 = "Delete from Laptop where id = ?";
+        crud.deleteLaptop(query4,3);
     }
 }
